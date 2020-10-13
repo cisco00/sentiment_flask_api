2caa03936128bf3018bcdad7d09e73f017bee3a4
cisco00 <idokofrancis66@gmail.com>
2020-10-14 00:43:42 +0100

    update

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from sklearn.base import BaseEstimator, ClassifierMixin
+# from sklearn.base import BaseEstimator, ClassifierMixin
+from collections import Counter
 import pandas as pd
 import numpy as np
+from sklearn.base import ClassifierMixin, BaseEstimator
 
-data = pd.read_csv('imbd.csv', delimiter ='\t', names=['review', 'rating'])
-y = data['review']
-x = data['rating']
 
-class model(BaseEstimator, ClassifierMixin):
-    def gaussianNB(self):
+class MajorityClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self):
         pass
 
-    def fit(self, X, y = None):
+    def fit(self, X, y):
+        c = Counter(y)
+        self.mode = c.most_common(1)[0][0]  ##getting the most common features in the data set
+        self.fraction = np.array(list(c.values())) / X.shape[0]  ##getting the list of all the values
+
         return self
 
-        
+    def predict(self, X):
+        return self.mode_ * np.ones(X.shape[0])
 
-
+    def predict_proba(self, X):
+        np.dot(np.ones(X.shape[0], 1)), self.fraction.reshape(-1, 3)
